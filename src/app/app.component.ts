3c5b637db611ba34222ac6cd5384083d3931943b
ANA BEATRIZ RODRIGUES LOPES <607287469@alunos.etec.itaqua>
2019-10-21 20:29:31 -0300

    completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  navegacao : any;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
+    this.sideMenu();
     this.initializeApp();
   }
 
@@ -23,4 +25,31 @@ export class AppComponent {
       this.splashScreen.hide();
     });
   }
+
+  sideMenu()
+  {
+    this.navegacao =
+    [
+      {
+        title : "Home",
+        url   : "/home",
+        icon  : "home"
+      },
+      {
+        title : "Chat",
+        url   : "/chat",
+        icon  : "chatboxes"
+      },
+      {
+        title : "Contatos",
+        url   : "/contatos",
+        icon  : "contacts"
+      },
+      {
+        title : "Logout",
+        url   : "/login",
+        icon  : "log-out"
+      },
+    ]
+  }  
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+data: any;
+usuario:any;
+senha:any;
 
-  constructor() {}
+ 
+constructor(private route: ActivatedRoute, private router: Router) {
+  this.route.queryParams.subscribe(params => {
+    if (params && params.special) {
+      this.data = JSON.parse(params.special);
+      console.log(params.special);      
+      console.log(this.data);
+      this.usuario = this.data.username;
+      this.senha = this.data.password;
+      console.log(this.usuario);
+      console.log(this.senha);      
 
+
+    }
+  });
+};
 }
